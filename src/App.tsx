d3ab9a91eff88d3b3e0f2d8c81476aa7362fa6a1
Taskin Abdur-Rahman <72671675+navidr08-hub@users.noreply.github.com>
2024-01-09 04:04:04 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,40 @@
-import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
-import Landing from "./routes/Landing";
-import Login from "./routes/Login";
-import Signup from "./routes/Signup";
+function App() {
+  const [count, setCount] = useState(0);
 
-import RequireAuth from "./components/RequireAuth";
-
-export default function App() {
   return (
-    <Routes>
-      <Route index element={<Landing />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-    </Routes>
+    <div className="App">
+      <div>
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>React + Vite</h1>
+      <h2>On CodeSandbox!</h2>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR.
+        </p>
+
+        <p>
+          Tip: you can use the inspector button next to address bar to click on
+          components in the preview and open the code in the editor!
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
   );
 }
+
+export default App;
