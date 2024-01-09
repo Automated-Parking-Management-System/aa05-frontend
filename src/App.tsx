2c0154140120340e756991af35dfe827cc0cde14
Taskin Abdur-Rahman <72671675+navidr08-hub@users.noreply.github.com>
2024-01-09 04:06:05 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
-import { Routes, Route } from "react-router-dom";
 
-import Landing from "./routes/Landing";
-import Login from "./routes/Login";
-import Signup from "./routes/Signup";
 
-import RequireAuth from "./components/RequireAuth";
-
-export default function App() {
+function App() {
   return (
-    <Routes>
-      <Route index element={<Landing />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-    </Routes>
   );
 }
+
+export default App;
