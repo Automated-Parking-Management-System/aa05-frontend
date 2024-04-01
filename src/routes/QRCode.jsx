c81a350e811ac53b382bcfdeb9e437eb2c433015
Yadu-M <ymadhu@torontomu.ca>
2024-04-01 15:44:26 -0400

    now adds qr code to rtdb and fetches qr code status

--- src/routes/QRCode.jsx
+++ src/routes/QRCode.jsx
@@ -1,7 +1,8 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
-import { Paper } from "@mui/material";
+import { Paper, Typography } from "@mui/material";
+import { getDatabase, ref, set, onValue } from "firebase/database";
 import NavBar from "../components/NavBar";
 
 import Header from "../components/Header";
@@ -16,19 +17,60 @@ const style = {
   overflow: "auto",
 };
 
+
 const QRCode = () => {
   const { currentUser } = useContext(AuthContext);
   const { lotId } = useParams();
   const [code, setCode] = useState("Example");
   const [src, setSrc] = useState("");
+  const [verification, setVerification] = useState(false);
+  const rtdb = getDatabase();
+
+  const updateRTDB = (obj) => {
+    set(ref(rtdb, 'QR-Code/'), obj);
+  }
+
+  const getRTDB = (path) => {
+    const verifyCheck = ref(rtdb, 'QR-Code/' + path);
+    let isSucess = false;
+    onValue(verifyCheck, (snapshot) => {
+      const data = snapshot.val();
+      if (data === false) {
+        // console.log('succcess (for now)');
+        isSucess = true;
+        // throw new Error("CAM didnt verify");  // uncomment for the future when firebase has been integrated
+        try {
+          set(ref(rtdb, 'QR-Code/' + path), true);  // Setting it true for now, CAM shud check
+        } catch (error) {
+          isSucess = false;
+          throw new Error("Doesnt matter what happens here tbh");
+        }
+      }
+    });
+
+    return isSucess;    
+  }
 
   const generateQRCode = () => {
-    const url = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${code}`;
+    const url = `https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=${code}`;
     const original = currentUser.uid + "+" + lotId;
     const shuffled = shuffle(original);
     setCode(shuffled);
     setSrc(url);
+
+    // Updating rtdb
+    const qrObj = {};
+    qrObj[shuffled] = false;
+    updateRTDB(qrObj);
+
+    try {
+      if (getRTDB(shuffled) === undefined) throw new Error("Something went wrong ...");
+      else setVerification(true);
+    } catch (error) {
+      alert(error);
+    }
   };
+
 
   useEffect(() => {
     generateQRCode();
@@ -36,11 +78,12 @@ const QRCode = () => {
 
   return (
     <Paper style={{ maxHeight: "100%" }}>
-      <Header title={"QR-Code"} />
-      <Paper sx={style} elevation={3}>
+      {!verification && <Typography variant="h5" >QR-Code Generated </Typography>}
+      {!verification && <Typography variant="h6" >Show it to the camera </Typography>}
+      {verification && <Typography variant="h5">Gate verification successful !</Typography>}
+      {!verification && <Paper sx={style} elevation={3}>
         <img style={{ margin: "5%" }} src={src}></img>
-      </Paper>
-      <NavBar nav={"home"} />
+      </Paper>}
     </Paper>
   );
 };
