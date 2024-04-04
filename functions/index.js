95131a90b2c95bbf9b4dfecf52f99e276a3ccb54
Yadu-M <ymadhu@torontomu.ca>
2024-04-03 22:51:28 -0400

    added qr code decode and rtdb qr code verification

--- functions/index.js
+++ functions/index.js
@@ -7,49 +7,53 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
+// @ts-nocheck
+
+
 const { onRequest } = require("firebase-functions/v2/https")
 const { onObjectFinalized } = require("firebase-functions/v2/storage");
+// const rtdb = require("firebase/database")
 const logger = require("firebase-functions/logger");
 const admin = require("firebase-admin");
+const Jimp = require("jimp");
+const jsQR = require("jsqr");
+const { set } = require("firebase/database");
 
-// Random libraries i tried
-// const jsqr = require("jsqr")
-
-// const Jimp = require("jimp");
 
 admin.initializeApp();
-
+const db = admin.database();
 
 // Listens for any stuff that gets added to storage and then runs the call back
 exports.detectQR_code = onObjectFinalized(async (event) => {
   if (event.data.contentType.startsWith("image/")) { // Checking if whatevr was added was an image
     const bucket = admin.storage().bucket(event.data.bucket);
     const file = bucket.file(event.data.name);
-
-    // Download the image file
     const tempFilePath = `/tmp/${event.data.name}`;
-    await file.download({ destination: tempFilePath });
 
-    //-----------------Commented this out, TODO: Decode image here------------------------------#
-    // // Use Jimp to read the image and convert it to a bitmap
-    // const image = await Jimp.read(tempFilePath);
-    // const { data, width, height } = image.bitmap;
+    await file.download({ destination: tempFilePath })
 
-    // // Create a Uint8ClampedArray from the image data
-    // const imageData = {
-    //   data: new Uint8ClampedArray(data),
-    //   width,
-    //   height,
-    // };
+    const image = await Jimp.read(tempFilePath);
 
-    // // Use jsqr to decode the QR code from the image data
-    // const qrCode = jsqr(imageData.data, imageData.width, imageData.height);
-    // logger.log(qrCode);
-    // if (qrCode) {
-    //   logger.log("QR code detected:", qrCode.data);
-    //  else {
-    //   logger.log("No QR code detected.");
-    // }
+    const qrCodeValue = jsQR(image.bitmap.data, image.bitmap.width, image.bitmap.height);
+
+    if (qrCodeValue) {
+      const qrCodeDataRef = db.ref(`QR-Code/${qrCodeValue.data}`);
+    
+      qrCodeDataRef.get()
+        .then(snapshot => {
+          const qrCodeStatus = snapshot.val();
+    
+          if (qrCodeStatus === null) {
+            throw new Error("Couldn't fetch the code");
+          } else if (qrCodeStatus === false) {
+            return qrCodeDataRef.set(true); // Setting the value directly
+          }
+        })
+        .catch((err) => logger.error(err));
+    } else {
+      console.log('No QR code found in the image.');
+    }
+
   } else {
     logger.log("Object is not an image.");
   }
