66b9b42d8f0d1934d22d361c8dfbaeafe158bc2c
Yadu-M <ymadhu@torontomu.ca>
2024-04-03 22:45:36 -0400

    added the databaseurl (dont need it currently so commented out)

--- src/firebase/FirebaseConfig.jsx
+++ src/firebase/FirebaseConfig.jsx
@@ -1,11 +1,13 @@
 const config = {
   apiKey: "AIzaSyC129F4OIPEJUjGMbrEu9rUYMeKoo_zBY8",
   authDomain: "aa05-apms.firebaseapp.com",
+  // databaseURL: "https://aa05-apms-default-rtdb.firebaseio.com",
   projectId: "aa05-apms",
   storageBucket: "aa05-apms.appspot.com",
   messagingSenderId: "16178663204",
   appId: "1:16178663204:web:aab59eab625cb07a6fadb1",
-  measurementId: "G-1WYSSXCS1M",
+  measurementId: "G-1WYSSXCS1M"
+
 };
 
 export function getFirebaseConfig() {
