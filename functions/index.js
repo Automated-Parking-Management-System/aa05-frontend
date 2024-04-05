286a1cf75a35ef42d1f3b97e934c5473476fa640
Yadu-M <ymadhu@torontomu.ca>
2024-04-04 20:10:26 -0400

    removed some unused stuff, tried saving image if cloud function detected qr code

--- functions/index.js
+++ functions/index.js
@@ -7,42 +7,43 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-// @ts-nocheck
 
-
-const { onRequest } = require("firebase-functions/v2/https")
+const { onRequest } = require("firebase-functions/v2/https");
 const { onObjectFinalized } = require("firebase-functions/v2/storage");
-// const rtdb = require("firebase/database")
 const logger = require("firebase-functions/logger");
 const admin = require("firebase-admin");
 const Jimp = require("jimp");
 const jsQR = require("jsqr");
-const { set } = require("firebase/database");
-
+const { bucket } = require("firebase-functions/v1/storage");
 
 admin.initializeApp();
 const db = admin.database();
 
 // Listens for any stuff that gets added to storage and then runs the call back
 exports.detectQR_code = onObjectFinalized(async (event) => {
+  const bucket = admin.storage().bucket(event.data.bucket); 
+  const file = bucket.file(event.data.name);
+  let qrDetected = false;
+
   if (event.data.contentType.startsWith("image/")) { // Checking if whatevr was added was an image
-    const bucket = admin.storage().bucket(event.data.bucket);
-    const file = bucket.file(event.data.name);
     const tempFilePath = `/tmp/${event.data.name}`;
 
-    await file.download({ destination: tempFilePath })
+    await file.download({ destination: tempFilePath });
 
     const image = await Jimp.read(tempFilePath);
 
-    const qrCodeValue = jsQR(image.bitmap.data, image.bitmap.width, image.bitmap.height);
+    const qrCodeValue = jsQR(image.bitmap.data, image.bitmap.width, image.bitmap.height); // qr code obj
 
-    if (qrCodeValue) {
-      const qrCodeDataRef = db.ref(`QR-Code/${qrCodeValue.data}`);
+    if (qrCodeValue !== null) {
+
+      const qrCodeDataRef = db.ref(`QR-Code/${qrCodeValue.data}`);  //.data is the actual value
     
+      // This is updating the rtdb value
       qrCodeDataRef.get()
         .then(snapshot => {
           const qrCodeStatus = snapshot.val();
-    
+          qrDetected = true;
+
           if (qrCodeStatus === null) {
             throw new Error("Couldn't fetch the code");
           } else if (qrCodeStatus === false) {
@@ -57,14 +58,17 @@ exports.detectQR_code = onObjectFinalized(async (event) => {
   } else {
     logger.log("Object is not an image.");
   }
+
+  if (!qrDetected) {  // Trying to delete images that were not detected as qr codes
+    try {
+      await file.delete();  
+    } catch (error) {
+      logger.log(error);
+    }
+  } 
+  
 });
 
-
-// Simple listener
-exports.helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
 
 // Listener just returns the number of files in storage
 exports.getStorageFileCount = onRequest(async (request, response) => {
