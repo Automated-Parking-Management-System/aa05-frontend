2f81f516d7bbd2fc23e3b2c85f8d174c1e03ad11
Yadu-M <ymadhu@torontomu.ca>
2024-04-06 02:59:51 -0400

    cleaned up function, now unshuffles incoming data and updates the auth state in the correct path in rtdb.

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,6 @@
 /**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ * 
+ * Firebase Cloud Functions
  */
 
 
@@ -16,8 +12,23 @@ const Jimp = require("jimp");
 const jsQR = require("jsqr");
 const { bucket } = require("firebase-functions/v1/storage");
 
-admin.initializeApp();
+const admin_app = admin.initializeApp();
 const db = admin.database();
+const secret_key = "JdeQsCeNA1T3ZQaUtQhcHGf1s343";
+
+
+function unshuffle(reversed) {
+  let str = reversed.split('');
+
+  for (let i = str.length - 1; i >= 1; i--) {
+      for (let j = str.length - 1; j >= i; j--) {
+          let temp = str[j];
+          str[j] = str[j - 1];
+          str[j - 1] = temp;
+      }
+  }
+  return str.join('');
+}
 
 // Listens for any stuff that gets added to storage and then runs the call back
 exports.detectQR_code = onObjectFinalized(async (event) => {
@@ -27,30 +38,34 @@ exports.detectQR_code = onObjectFinalized(async (event) => {
 
   if (event.data.contentType.startsWith("image/")) { // Checking if whatevr was added was an image
     const tempFilePath = `/tmp/${event.data.name}`;
-
     await file.download({ destination: tempFilePath });
-
     const image = await Jimp.read(tempFilePath);
-
     const qrCodeValue = jsQR(image.bitmap.data, image.bitmap.width, image.bitmap.height); // qr code obj
 
+
     if (qrCodeValue !== null) {
+      
+      let verify = false;
+      qrDetected = true;
+      
+      const decoded_qr_code = unshuffle(qrCodeValue.data);
+      const users = await admin_app.auth().listUsers(1000);
 
-      const qrCodeDataRef = db.ref(`QR-Code/${qrCodeValue.data}`);  //.data is the actual value
-    
-      // This is updating the rtdb value
-      qrCodeDataRef.get()
-        .then(snapshot => {
-          const qrCodeStatus = snapshot.val();
-          qrDetected = true;
-
-          if (qrCodeStatus === null) {
-            throw new Error("Couldn't fetch the code");
-          } else if (qrCodeStatus === false) {
-            return qrCodeDataRef.set(true); // Setting the value directly
-          }
+      for (let user of users.users) {
+        if (decoded_qr_code.includes(user.uid)) verify = true;
+      }
+      
+      if (verify) {
+        const qrCodeDataRef = db.ref(`${secret_key}/entranceAuth`);  
+        
+        // Sets the entrance auth to true
+        await qrCodeDataRef.set(true)
+        .catch(e => {
+          logger.error(e);
         })
-        .catch((err) => logger.error(err));
+        
+      }
+      
     } else {
       console.log('No QR code found in the image.');
     }
